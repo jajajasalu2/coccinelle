15940270d5850aefaf458dcca69202e89d80ea66
Jaskaran Singh <jaskaransingh7654321@gmail.com>
2020-03-03 18:51:59 +0530

    parsing_cocci: disjdistr: Add cases for ParenType/FunctionType
    ParenType and FunctionType are now added to the SmPL ASTs. Add
    cases for these types in disjdistr.ml.
    
    Signed-off-by: Jaskaran Singh <jaskaransingh7654321@gmail.com>

--- parsing_cocci/disjdistr.ml
+++ parsing_cocci/disjdistr.ml
@@ -73,6 +73,16 @@ and disjtypeC bty =
 	(function ty ->
 	  Ast.rewrap bty (Ast.FunctionPointer(ty,lp1,star,rp1,lp2,params,rp2)))
 	ty
+  | Ast.ParenType(lp,ty,rp) ->
+      let ty = disjty ty in
+      List.map
+        (function ty ->
+          Ast.rewrap bty (Ast.ParenType(lp,ty,rp))) ty
+  | Ast.FunctionType(ty,lp,params,rp) ->
+      let ty = disjty ty in
+      List.map
+        (function ty ->
+          Ast.rewrap bty (Ast.FunctionType(ty,lp,params,rp))) ty
   | Ast.Array(ty,lb,size,rb) ->
       disjmult2 (disjty ty) (disjoption disjexp size)
 	(function ty -> function size ->
