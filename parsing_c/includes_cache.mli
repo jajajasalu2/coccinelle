7983e3ead10307ee10fafbdd83efec54ea5eaee0
Jaskaran Singh <jaskaran.singh@collabora.com>
2020-08-29 14:41:35 +0530

    includes_cache
    Signed-off-by: Jaskaran Singh <jaskaran.singh@collabora.com>

---
+++ parsing_c/includes_cache.mli
@@ -0,0 +1,26 @@
+(*
+ * This file is part of Coccinelle, licensed under the terms of the GPL v2.
+ * See copyright.txt in the Coccinelle source code for more information.
+ * The Coccinelle source code can be obtained at http://coccinelle.lip6.fr
+ *)
+
+val print_hashtable : unit -> unit
+
+val has_been_parsed : string -> bool
+
+val add_to_parsed_files : string -> unit
+
+val add_to_dependency_graph : string -> string -> unit
+
+val path_exists_dependency_graph : Common.filename -> Common.filename -> bool
+
+val extract_names : Common.filename -> Ast_c.program -> unit
+
+val print_dependency_graph : unit -> unit
+
+val get_type_from_name_cache :
+  Common.filename ->
+    string ->
+    (Common.filename -> Ast_c.program) ->
+    Ast_c.exp_info option
+
